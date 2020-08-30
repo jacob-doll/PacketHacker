7f100682791ae861084d29ad0ab1e1bd51dc5a93
Jacob Doll <jacob.g.doll@gmail.com>
2020-08-30 11:49:53 -0400

    Began implementation of routing tables
    - Added routing table class

---
+++ src/packet/platform/win32routing_table.cpp
@@ -0,0 +1,1 @@
+#include "packet/routing_table.h"
---
+++ src/packet/routing_table.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "ip_address.h"
+
+#include <vector>
+
+namespace PacketHacker {
+
+struct RouteEntry
+{
+  IPv4Address networkDest;
+  IPv4Address netmask;
+  IPv4Address nextHop;
+  uint32_t metric;
+};
+
+class RoutingTable
+{
+public:
+  RoutingTable(const uint32_t ifIndex);
+
+  void RefreshTable();
+
+  void AddEntry(const IPv4Address &networkDest,
+    const IPv4Address &netmask,
+    const IPv4Address &nextHop,
+    const uint32_t metric);
+  void DeleteEntry(const IPv4Address &networkDest);
+  IPv4Address &GetNextHop(const IPv4Address &networkDest);
+
+private:
+  const uint32_t m_ifIndex;
+  std::vector<RouteEntry> m_entries;
+};
+
+}// namespace PacketHacker
